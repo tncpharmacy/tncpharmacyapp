1e48a18730644f280e93d7839aab45ae330f5d8c
demo <pulkit726.@gmail.com>
2026-01-06 11:58:43 +0530

    06-jan

--- src/app/pharmacist/purchase-invoice/export/page.tsx
+++ src/app/pharmacist/purchase-invoice/export/page.tsx
@@ -197,7 +197,7 @@ export default function PurchaseInvoiceExport() {
         "Purchase Rate": "",
         Amount: "",
         Location: "",
-        applied_discount: "",
+        "Applied Discount": "",
       }));
 
     // 🟢 IMPORTANT: Wait until file export completes
