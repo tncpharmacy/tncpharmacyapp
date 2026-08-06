a1a62e6574b12f09cd7dcc902027c0a26ef43c97
tncpharmacy <tncpharmacy25@gmail.com>
2026-08-06 09:30:22 +0530

    06-08-2026

--- src/app/buyer-signup/page.tsx
+++ src/app/buyer-signup/page.tsx
@@ -349,7 +349,7 @@ export default function BuyerSignupModal({
               {/* {message && <p style={{ color: "green" }}>{message}</p>} */}
               {serverOtp && (
                 <p className="text-muted mt-2" style={{ fontSize: "13px" }}>
-                  {/* (Debug OTP: {serverOtp}) */}
+                  (Debug OTP: {serverOtp})
                 </p>
               )}
             </div>
